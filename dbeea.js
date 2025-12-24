ba56e086822d35c29ed4e7d8d0ee0d71fa2d94b2
Charanjeet Singh Sidhu <62802350+cssidhu0001@users.noreply.github.com>
2025-12-24 21:16:49 +0530

    Final Commit before host

--- dbeea.js
+++ dbeea.js
@@ -253,3 +253,21 @@ const observer = new IntersectionObserver(
 );
 
 observer.observe(section);
+
+// Scroll Section
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
